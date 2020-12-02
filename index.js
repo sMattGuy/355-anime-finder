be7d6191a52ae368ace6d69175fa539ed63f061f
MattGuy2 <matthewfrankflammia@gmail.com>
2020-12-01 22:59:56 -0500

    working with authentication, needs to be tested throughly

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-/*
-=-=-=-=-=-=-=-=-=-=-=-=-
-Album Art Search
-=-=-=-=-=-=-=-=-=-=-=-=-
-Student ID:
-Comment (Required):
-
-=-=-=-=-=-=-=-=-=-=-=-=-
-*/
 const fs = require('fs');
 const url = require("url");
 const http = require('http');
@@ -17,11 +8,20 @@ const credentials = require('./auth/credentials.json');
 const port = 3000;
 const server = http.createServer();
 
+let animeIDPass = 0;
 server.on("request", connection_handler);
 function connection_handler(req, res){
 	console.log(`New Request for ${req.url} from ${req.socket.remoteAddress}`);
-	let animeIDPass = 0;
 	if(req.url === "/"){
+		//let the jokes begin
+		let fakePerson = https.get('https://thispersondoesnotexist.com/image', function(res){
+			let fakeSave = fs.createWriteStream('./cache/fakePerson.jpg',{'encoding':null});
+			res.pipe(fakeSave);
+			fakeSave.on("finish",function(){
+				console.log("Imaginary Person Saved Succesfully");
+			});
+			fakeSave.on('error',function(err){console.log(err)});
+		});
 		const main = fs.createReadStream("html/main.html");
       res.writeHead(200, {"Content-Type": "text/html"});
       main.pipe(res);
@@ -41,67 +41,116 @@ function connection_handler(req, res){
       const image_stream = fs.createReadStream("images/chika.gif");
 		image_stream.pipe(res);
    }
+	else if (req.url.startsWith("/cache/")){
+      let image_stream = fs.createReadStream(`.${req.url}`);
+		image_stream.on("error",image_error_handler);
+		function image_error_handler(err){
+			res.writeHead(404, {"Content-Type":"text/plain"});
+			res.write("404 Not Found", () => res.end());
+		}
+		image_stream.on("ready", deliver_image);
+		function deliver_image(){
+			res.writeHead(404, {"Content-Type":"text/plain"});
+			image_stream.pipe(res);
+		}
+   }
 	else if (req.url.startsWith("/search")){
 		const user_input = url.parse(req.url, true).query;
-		const anime = user_input.anime;
-		//getting information from trace.moe
-		const whatanimeurl = `https://trace.moe/api/search?url=${anime}`;
-		let animeReq = https.get(whatanimeurl, function(animeReq){
-			stream_to_message(animeReq, message => whatanimeresults(message, res));
-		});
-		//parse response from what anime
-		function whatanimeresults(message, res){
-			let whatanimejson = JSON.parse(message);
-			//grabbing english title
-			let title = whatanimejson.docs[0].title_english;
-			console.log(title);
-			//creating api request
-			let anilisturl = 'https://graphql.anilist.co'
-			let variables = {"title":`${title}`};
-			let reqData = JSON.stringify({
-				'query':'query($title:String){Media(search:$title,type:ANIME){id episodes genres popularity}}',
-				variables
-			});
-			console.log(reqData);
-			let options = {
-				'method':'POST',
-				'headers':{
-					'Content-Type':'application/json',
-					'Accept':'application/json',
-				},
+		function isValidUrl(string) {
+			try {
+				new URL(string);
+			} catch (_) {
+				return false;  
 			}
-			//requesting information
-			let anilistReq = https.request(anilisturl,options);
-			anilistReq.on('error',error_handler);
-			function error_handler(err){
-				throw err;
-			}
-			anilistReq.once('response', post_auth_cb);
-			function post_auth_cb(incoming_msg_stream){
-				stream_to_message(incoming_msg_stream, message => createAniListInfo(message, res));
-			}
-			anilistReq.end(reqData);
-			//use anilist data
-			function createAniListInfo(message, res){
-				let aniListInfo = JSON.parse(message);
-				let animeID = aniListInfo.data.Media.id;
-				animeIDPass = animeID;
-				let episodes = aniListInfo.data.Media.episodes;
-				let genre = aniListInfo.data.Media.genres;
-				let popularity = aniListInfo.data.Media.popularity;
-				let aniListFinalUrl = `https://anilist.co/anime/${animeID}`;
-				//let the jokes begin
-				let authURL = "https://anilist.co/api/v2/oauth/authorize?"
-				let authInfo = {
-					"client_id":`${credentials.id}`,
-					"redirect_uri":"http://localhost:3000",
-					
+			return true;
+		}
+		//sees if user entered a url
+		if(isValidUrl(user_input.anime)){
+			console.log("Valid Url");
+			const anime = user_input.anime;
+			let photoname = anime.substring(anime.lastIndexOf('/')+1);
+			//sees if url is an image
+			if(photoname.includes(".jpg")||photoname.includes(".png")||photoname.includes(".jpeg")){
+				console.log("Valid Photo");
+				//downloads the image into cache
+				let filepath = './cache/';
+				let fullpath = `${filepath}${photoname}`;
+				if(!fs.existsSync(fullpath)){
+					let imgReq = https.get(anime, function(res){
+						let newImg = fs.createWriteStream(fullpath,{'encoding':null});
+						res.pipe(newImg);
+						newImg.on("finish",function(){
+							console.log("Upload Image Saved Succesfully");
+						});
+						newImg.on('error',function(err){console.log(err)});
+					});
 				}
-				//begin webpage creation 
-				console.log("generating final page");
-				res.writeHead(200, {"Content-Type":"text/html"});
-				//creating a webpage inline oh god
-				res.end(`
+				//getting information from trace.moe
+				const whatanimeurl = `https://trace.moe/api/search?url=${anime}`;
+				let animeReq = https.get(whatanimeurl, function(animeReq){
+					stream_to_message(animeReq, message => whatanimeresults(message, res));
+				});
+				//parse response from what anime
+				function whatanimeresults(message, res){
+					let whatanimejson = JSON.parse(message);
+					//grabbing english title
+					let title = whatanimejson.docs[0].title_english;
+					console.log(title);
+					//creating api request
+					let anilisturl = 'https://graphql.anilist.co'
+					let variables = {"title":`${title}`};
+					let reqData = JSON.stringify({
+						'query':'query($title:String){Media(search:$title,type:ANIME){id episodes genres popularity}}',
+						variables
+					});
+					console.log(reqData);
+					let options = {
+						'method':'POST',
+						'headers':{
+							'Content-Type':'application/json',
+							'Accept':'application/json',
+						},
+					}
+					//requesting information
+					let anilistReq = https.request(anilisturl,options);
+					anilistReq.on('error',error_handler);
+					function error_handler(err){
+						throw err;
+					}
+					anilistReq.once('response', post_auth_cb);
+					function post_auth_cb(incoming_msg_stream){
+						stream_to_message(incoming_msg_stream, message => createAniListInfo(message, res));
+					}
+					anilistReq.end(reqData);
+					//use anilist data
+					function createAniListInfo(message, res){
+						let aniListInfo = JSON.parse(message);
+						let animeID = aniListInfo.data.Media.id;
+						animeIDPass = animeID;
+						let episodes = aniListInfo.data.Media.episodes;
+						let genre = aniListInfo.data.Media.genres;
+						let popularity = aniListInfo.data.Media.popularity;
+						let aniListFinalUrl = `https://anilist.co/anime/${animeID}`;
+						const animeInfo = `./cache/${animeID}.json`;
+						if(!fs.existsSync(animeInfo)){
+							let filedata = {
+								"title":`${title}`,
+								"id":`${animeID}`,
+								"episodes":`episodes`,
+								"genre":`${genre}`,
+								"popularity":`${popularity}`
+							};
+							let inputData = JSON.stringify(filedata);
+							fs.writeFile(animeInfo,inputData, (err) => {
+								if(err) throw err;
+								console.log("File Written Succesfully");
+							});
+						}
+						//begin webpage creation 
+						console.log("generating final page");
+						res.writeHead(200, {"Content-Type":"text/html"});
+						//creating a webpage inline oh god
+						res.end(`
 <!DOCTYPE html>
 <html>
 	<head>
@@ -120,27 +169,112 @@ function connection_handler(req, res){
 	</head>
 	<body>
 		<h1>Your photo came up as ${title}</h1>
-		<h3>${title} is a ${genre} anime with ${episodes} episodes. Its popularity comes in at ${popularity}.</h3>
-		<a href = "${title}"><p>Link to AniList Page</p></a>
-		<img src="https://thispersondoesnotexist.com/image" style="width:120px;float:left;"/><p>Interested in adding this show to your list? <a href="https://anilist.co/api/v2/oauth/authorize?client_id=${credentials.id}&response_type=token">Click this link to do so!</a></p>
+		<img src="./cache/${photoname}" style="width:100px;padding:10px;float:left;"><p>(Your photo for reference)</p>
+		<h3 style="clear:left;">${title} is a ${genre} anime with ${episodes} episodes. Its popularity comes in at ${popularity}.</h3>
+		<a href = "${aniListFinalUrl}"><p>Link to AniList Page</p></a>
+		<img src="./cache/fakePerson.jpg" style="width:120px;float:left;padding:10px"/><p style="font-family: 'Bradley Hand', cursive;">I love this show, Interested in adding this show to your list? <a href="https://anilist.co/api/v2/oauth/authorize?client_id=${credentials.id}&response_type=token">Click this link to do so!</a></p>
 	</body>
 </html>
-				`);
+						`);
+					}
+				}
+				//parse response from https call
+				function stream_to_message(stream, callback){
+					let body = "";
+					stream.on("data", (chunk) => body += chunk);
+					stream.on("end", () => callback(body));
+				}
+			}
+			else{
+				res.writeHead(400, {"Content-Type": "text/html"});
+				res.end(`<h1>400 Requested URL Not An Image</h1>`);
 			}
 		}
+		else{
+			res.writeHead(400, {"Content-Type": "text/html"});
+			res.end(`<h1>400 Invalid URL Entered</h1>`);
+		}
+	}
+	//gets auth token
+	else if (req.url.startsWith("/authorized?")){
+		const accessInfo = url.parse(req.url, true).query;
+		const token = accessInfo.access_token;
+		let endpoint = 'https://graphql.anilist.co';
+		let options = {
+			"method": "POST",
+			"headers": {
+				"Authorization": `Bearer ${token}`,
+				"Content-Type":"application/json",
+				"Accept":"application/json"
+			}
+		}
+		let query = 'mutation($mediaId:Int, $status: MediaListStatus){SaveMediaListEntry(mediaId: $mediaId, status: $status){id status}}';
+		let variables = {"mediaId":`${animeIDPass}`,"status":"PLANNING"};
+		let reqData = JSON.stringify({
+			query,
+			variables
+		});
+		console.log(reqData);
+		//requesting information
+		let anilistReq = https.request(endpoint,options);
+		anilistReq.on('error',error_handler);
+		function error_handler(err){
+			throw err;
+		}
+		anilistReq.once('response', post_auth_cb);
+		function post_auth_cb(incoming_msg_stream){
+			stream_to_message(incoming_msg_stream, message => addedToList(message, res));
+		}
+		anilistReq.end(reqData);
 		//parse response from https call
 		function stream_to_message(stream, callback){
 			let body = "";
 			stream.on("data", (chunk) => body += chunk);
 			stream.on("end", () => callback(body));
 		}
-   }
+		function addedToList(message, res){
+			let resultsInfo = JSON.parse(message);
+			let id = resultsInfo.mediaId;
+			let status = resultsInfo.status;
+			let cache = `./cache/${animeIDPass}`;
+			let animeInfo = require(cache);
+			res.writeHead(200, {"Content-Type": "text/html"});
+			res.end(`
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Anime Finder</title>
+		<style>
+			body, form{
+				margin: 0 auto;
+				max-width:652px;
+				overflow-x:hidden;
+				background-color:#CCCCFF;
+			}
+			fieldset{
+				display: flex;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>${animeInfo.title} has been added to your list!</h1>
+		<a href ="https://anilist.co/anime/${animeInfo.id}"><p>Link to the show</p></a>
+		<img src="./cache/fakePerson.jpg" style="width:120px;float:left;padding:10px"/><p style="font-family: 'Bradley Hand', cursive;">Thank you for adding this show, you'll love it.</p>
+	</body>
+</html>
+			`);
+		}
+	}
 	//authorization to add to list
 	else if (req.url.startsWith("/authorized")){
-		const token = req.headers.cookie;
-		console.log(token);
-		res.writeHead(404, {"Content-Type": "text/html"});
-      res.end(`<h1>Not yet implementated</h1>`);
+		res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(`
+			<script>
+			let url = window.location.href;
+			url = url.replace("#","?");
+			window.location.replace(url);
+			</script>
+		`);
 	}
    else{
       res.writeHead(404, {"Content-Type": "text/html"});
